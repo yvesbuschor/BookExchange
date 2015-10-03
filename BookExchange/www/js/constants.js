fa3c3f9110dedc9e1e80dfb9ae201d89a9d9760e
Yves Buschor <yves.buschor@gmail.com>
2015-10-03 22:43:24 +0200

    woking on authentication

---
+++ BookExchange/www/js/constants.js
@@ -0,0 +1,5 @@
+angular.module('bookexchange')
+
+.constant('CONST_VALUES', {
+	host: 'http://bookexchange.scapp.io'
+})
--- BookExchange/www/js/services.js
+++ BookExchange/www/js/services.js
@@ -1,4 +1,36 @@
 angular.module('bookexchange')
 
-.factory('bookApi', function($http, $q){
+.service('bookApi', function($http, $q){
+	var LOCAL_TOKEN = "myToken";
+	var isAuthenticated = false;
+	var authToken;
+
+	function loadUserCredentials() {
+		var token = window.localStorage.getItem(LOCAL_TOKEN);
+		if(token) {
+			useToken(token);
+		}
+	}
+
+	function storeUserCredentials(token) {
+		window.localStorage.setItem(LOCAL_TOKEN, token);
+		useToken(token);
+	}
+
+	function useToken(token) {
+		isAuthenticated = true;
+		authToken = token;
+		$http.defaults.headers.common['X-API'] = token;
+	}
+
+	function destroyToken() {
+		authToken = undefined;
+		isAuthenticated = false;
+		$http.defaults.headers.common['X-API'] = undefined;
+		window.localStorage.removeItem(LOCAL_TOKEN);
+	}
+
+
+
+
 });
